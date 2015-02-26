733df756c01f4bb93add61ffb4563b203d158206
Thomas Brand <tom@trellis.ch>
2015-02-26 01:04:24 +0100

    allow osx fullscreen mode test

--- src/ch/lowres/wavegraph/Mac.java
+++ src/ch/lowres/wavegraph/Mac.java
@@ -2,6 +2,9 @@
 
 package ch.lowres.wavegraph;
 
+import java.awt.Window;
+import java.lang.reflect.*;
+
 //========================================================================
 public class Mac
 {
@@ -13,8 +16,8 @@ public class Mac
 //========================================================================
 	public static void init()
 	{
-		//set name in osx menu
-		System.setProperty("com.apple.mrj.application.apple.menu.about.name",m.progName);
+		//set name in osx menu (only works if called before any widget created)
+		//System.setProperty("com.apple.mrj.application.apple.menu.about.name",m.progName);
 
 		//AboutHandler, PreferencesHandler, AppReOpenedListener, OpenFilesHandler, PrintFilesHandler, QuitHandler, QuitResponse 
 		com.apple.eawt.Application application = com.apple.eawt.Application.getApplication();
@@ -50,5 +53,28 @@ public class Mac
 
 		//only works with Aqua Look and Feel
 		//application.setDefaultMenuBar(m.applicationMenu);
+
+		enableFullScreenMode(m.mainframe);
+
 	}//end init
+
+//========================================================================
+	//http://saipullabhotla.blogspot.ch/2012/05/enabling-full-screen-mode-for-java.html
+	public static void enableFullScreenMode(Window window)
+	{
+		String className = "com.apple.eawt.FullScreenUtilities";
+		String methodName = "setWindowCanFullScreen";
+		try
+		{
+			Class<?> clazz = Class.forName(className);
+			Method method = clazz.getMethod(methodName, new Class<?>[]
+				{Window.class, boolean.class });
+			method.invoke(null, window, true);
+		}
+		catch(Throwable t)
+		{
+			System.err.println("Full screen mode is not supported");
+			t.printStackTrace();
+		}
+	}
 }//end class Mac
